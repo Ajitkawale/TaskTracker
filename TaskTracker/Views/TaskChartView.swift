dfbd8daf1e7f724981b493a61c01ee0aaa95cb1c
Ajit Kawale <ajitkawale27@gmail.com>
2025-11-11 19:12:05 +0530

    pre-finalised

---
+++ TaskTracker/Views/TaskChartView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskChartView.swift
+//  TaskTracker
+//
+//  Created by Ajit Kawale on 11/11/25.
+//
+
+import SwiftUI
+
+struct TaskChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskChartView()
+}
